8a9c89c8e47be9f9a77bee7cb38ef6da3bf3afeb
sofia <selizondo003@insite.4cd.edu>
2022-03-14 18:08:16 -0700

    added moving ball

---
+++ j/main.js
@@ -0,0 +1,40 @@
+
+
+//creating constants
+const container = document.querySelector(".container");
+const pageX = document.getElementById("x");
+const pageY = document.getElementById("y");
+
+//create function for pageX and pageY
+function updateDisplay(event){
+    pageX.innerHTML = event.pageX;
+    pageY.innerHTML = event.pageY;
+
+    //open the JS console to see the output
+    console.log(event.pageX, event.pageY);
+}
+
+//detect mousemove, mouseenter, mouseleave events and handle them
+document.addEventListener("mousemove", updateDisplay);
+container.addEventListener("mouseenter", changeColor);
+container.addEventListener("mouseleave", resetColor);
+
+function changeColor(event){
+    container.style.backgroundColor = "#abc";
+}
+
+function resetColor(event){
+    container.style.backgroundColor = "pink";
+}
+
+var dot = document.getElementById('dot');
+document.addEventListener('click', (e) => {
+    dot.style.transform = `translateY(${e.clientY - 25}px)`;
+    dot.style.transform += `translateX(${e.clientX - 25}px)`;
+    dot.style.opacity = 1;
+}, false);
+     
+
+
+
+
